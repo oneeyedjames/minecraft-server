0c02fd0965df970315475278cd4f2a38150aa7be
OneEyedJames <jascott@programmer.net>
2017-12-16 15:27:49 -0500

    Added new .bat script for windows

---
+++ server.bat
@@ -0,0 +1,10 @@
+@echo off
+
+cd servers\%1
+
+"%PROGRAMFILES%\Git\bin\sh.exe" --login -i -c "git pull"
+
+java -XX:+UseConcMarkSweepGC -jar ../../builds/spigot-1.12.2.jar --plugins ../../plugins
+
+"%PROGRAMFILES%\Git\bin\sh.exe" --login -i -c "git commit -am 'Savepoint'"
+"%PROGRAMFILES%\Git\bin\sh.exe" --login -i -c "git push"
